eeb0dd3608e29da5392fd5291a29004c5d07cb7e
James Brenton <freshlybakedcode@jamesbrenton.co.uk>
2018-01-02 17:17:12 +0000

    Container populates, can determine what has been clicked, can respond, can detect matching cards, can hide/reveal them based on class names

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,61 @@
-console.log(`We got a script here.`);
+//Create an array of tiles/data to match
+const numberOfPairs = 5;
+const pairData = [];
+const container = document.querySelector('.container');
+const delayBeforeTurnBack = 1500;
+let output = '';
+let currentCard = '';
+let currentCardId = '';
+let firstCardSelected = '';
+let firstCardSelectedId = '';
+
+for (let i = 0; i < numberOfPairs; i++) {
+    pairData.push(i, i);
+}
+
+//Generate an HTML output string and send to the container
+pairData.forEach((item) => {
+    output += `<span class="card" data-card-id=${item}>${item}</span>`;
+});
+container.innerHTML = output;
+
+//Set listeners on all cards
+const cards = document.querySelectorAll('.card');
+cards.forEach(card => card.addEventListener("click", handleCardSelected));
+
+function handleCardSelected(e) {
+    currentCard = e.path[0];
+    currentCardId = currentCard.dataset.cardId;
+    currentCard.classList.add('face-up'); //Add face-up class to reveal card
+    if (firstCardSelectedId === '') {
+        firstCardSelectedId = currentCardId;
+        firstCardSelected = currentCard;
+    } else {
+        checkIfMatch();
+    }
+}
+
+function checkIfMatch() {
+    if (firstCardSelectedId === currentCardId) {
+        //Match!
+        console.log(`Match! ${currentCardId} ${firstCardSelectedId}`);
+        currentCard.classList.add('matched');
+        firstCardSelected.classList.add('matched');
+        turnCardsBack();
+    } else {
+        //not a match
+        console.log(`Not a match! ${currentCardId} ${firstCardSelectedId}`);
+        setTimeout(() => { turnCardsBack() }, delayBeforeTurnBack); //Delay before turning cards back over
+    }
+    firstCardSelectedId = '';
+}
+
+function turnCardsBack() {
+    //Turn back cards which have the face-up class and not the matched class
+    let currentlyTurnedOverCards = document.querySelectorAll('.face-up');
+    currentlyTurnedOverCards.forEach(card => {
+        if (!card.classList.contains('matched')) {
+            card.classList.remove('face-up');
+        }
+    });
+}
