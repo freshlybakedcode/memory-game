3b6f756555b11cea268ba8e46c37bf0e0e0c1bb1
James Brenton <james@jamesbrenton.co.uk>
2018-01-03 14:33:09 +0000

    Polyfill for path

--- app/js/script.js
+++ app/js/script.js
@@ -11,6 +11,25 @@ let currentCardId = '';
 let firstCardSelected = '';
 let firstCardSelectedId = '';
 
+//Polyfill for e.path
+if (!('path' in Event.prototype)) {
+	Object.defineProperty(Event.prototype, 'path', {
+		get: function () {
+			const path = [];
+			let currentElem = this.target;
+			while (currentElem) {
+				path.push(currentElem);
+				currentElem = currentElem.parentElement;
+			}
+			if (path.indexOf(window) === -1 && path.indexOf(document) === -1)
+				path.push(document);
+			if (path.indexOf(window) === -1)
+				path.push(window);
+			return path;
+		}
+	});
+}
+
 function setUpGameBoard() {
     //Set up the board ready to play
     pairData = shuffle(pairData);
